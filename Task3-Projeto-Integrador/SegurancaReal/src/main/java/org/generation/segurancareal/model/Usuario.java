224ea3268c79cf3b933b7d63240c19cd7c709e9d
Yuxis445 <luffy2033@outlook.com>
2021-12-10 10:25:20 -0300

    model Usuario

---
+++ Task3-Projeto-Integrador/SegurancaReal/src/main/java/org/generation/segurancareal/model/Usuario.java
@@ -0,0 +1,80 @@
+package org.generation.segurancareal.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+    @NotBlank (message = "Por favor insira o Nome")
+    private String nome;
+
+    @NotBlank (message = "Por favor insira o e-mal")
+    private String email;
+
+    @NotBlank (message = "Por favor insira a senha de no minimo 6 caracteres")
+    @Size(min = 6)
+    private String senha;
+
+    @OneToMany(mappedBy = "postagem", cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("postagem")
+    private List<Postagem> postagem;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public List<Postagem> getPostagem() {
+        return postagem;
+    }
+
+    public void setPostagem(List<Postagem> postagem) {
+        this.postagem = postagem;
+    }
+    
+    
+}
