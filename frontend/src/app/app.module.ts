c001ddf063ac16f52eae78aff370cc78b16ff610
Joao Victor <luffy2033@outlook.com>
2022-01-20 08:25:38 -0300

    add component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ContatoComponent } from './contato/contato.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroComponent,
+    LoginComponent,
+    MenuComponent,
+    RodapeComponent,
+    InicioComponent,
+    ContatoComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
